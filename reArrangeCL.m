79741a133930b4dc263825c75b32832c2a98560f
blackmount8 <blackmount8@gmail.com>
2016-05-13 16:36:05 +0800

    20160513
    .

--- reArrangeCL.m
+++ reArrangeCL.m
@@ -1,14 +1,34 @@
 G.meshedUG = zeros(G.horizon, G.numofMG);
-G.meshedCL = zeros(G.horizon, G.numofMG;)
+G.meshedCL = zeros(G.horizon, G.numofMG);
 for j = 1:G.numofMG
    G.meshedUG(:,j)  = MG_Group{j,1}.result2G(:,1);
 end
-pp = sign(G.meshedUG);
-qq = sum(pp, 2);
-G.totalUG = sum(G.meshedUG, 2);
-G.totalUG
+allIn = G.meshedUG>0;
+allOut = G.meshedUG<0;
+allInSum = sum(allIn, 2);
+allOutSum = sum(allOut, 2);
+allInUG = allIn.*G.meshedUG;
+allOutUG = allOut.*G.meshedUG;
+totalUG = sum(G.meshedUG, 2);
+
 for i = 1:G.horizon
-   switch qq(i,1)
+	if totalUG(i)>0
+        if sum( allIn(i,:), 2 ) < G.numofMG
+            standard = sum(allOutUG(i,:))/allInSum(i,:) ; %Setting the changing reference
+        else %all >0
+            %Do nothing
+        end
+	elseif totalUG<0
+      standard = sum(allInUG(i,:))/allOutSum(i,:)  ;
+	else
+	end
+       
+end
+
+
+
+for i = 1:G.horizon
+   switch allInSum(i,1)
        case G.numofMG
            %Do nothing 
        otherwise
